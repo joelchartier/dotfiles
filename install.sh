634519dfab933dd8278e554cb114a0776835ef97
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2019-08-06 22:46:43 -0400

    Add TPM and color scheme

--- install.sh
+++ install.sh
@@ -3,9 +3,18 @@
 function installUpdateVundle() {
 	if [ -d "$HOME/.vim/bundle/Vundle.vim" ]; then
 		cd ~/.vim/bundle/Vundle.vim
-		git pull ~/.vim/bundle/Vundle.vim
+		git pull
 	else
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	fi
+}
+
+function installUpdateTpm() {
+	if [ -d "$HOME/.tmux/plugins/tpm" ]; then
+		cd ~/.tmux/plugins/tpm
+		git pull
+	else
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 	fi
 }
 
@@ -23,13 +32,16 @@ function installFiles() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	installFiles;
 	installUpdateVundle;
+	installUpdateTpm;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		installFiles;
 		installUpdateVundle;
+		installUpdateTpm
 	fi;
 fi;
 unset installFiles;
 unset installUpdateVundle;
+unset installUpdateTpm;
